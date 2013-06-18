f84e2aa5538045f65196a8500e0ee2d0609b86d1
John Ward <spyderman4g63@gmail.com>
2013-06-18 07:45:48 -0400

    initial commit

---
+++ 202-config.php
@@ -0,0 +1,17 @@
+<?php
+
+// ** Get username and password from AppFog instance 
+$services_json = json_decode(getenv("VCAP_SERVICES"),true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+
+$username = 
+$password = 
+$hostname = $mysql_config["hostname"];
+$port = $mysql_config["port"];
+
+// ** MySQL settings ** //
+$dbname = $mysql_config["name"];    		// AppFog DB Name
+$dbuser = $mysql_config["username"];   	// AppFog Username
+$dbpass = $mysql_config["password"];; 	// AppFog Password
+$dbhost = $mysql_config["hostname"].':'.$mysql_config["port"];    							// 99% chance you won't need to change this value
+$mchost = 'localhost';  								//this is the memcache server host, if you don't know what this is, don't touch it.
